7f97878981f59d4f4ff9b4f0500aae1e06ae3c00
lprimak <lenny@flowlogix.com>
2024-09-29 00:41:32 -0500

    working with maven 3

---
+++ src/main/java/com/flowlogix/starter/ArchetypeGenerator.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.flowlogix.starter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.cli.MavenCli;
+
+@Slf4j
+public class ArchetypeGenerator {
+    public int generate() {
+        MavenCli cli = new MavenCli();
+        String projectDirectory = "/tmp/project";
+        System.setProperty(MavenCli.MULTIMODULE_PROJECT_DIRECTORY, projectDirectory);
+        return cli.doMain(new String[] { "archetype:generate", "-DarchetypeGroupId=com.flowlogix.archetypes",
+                "-DarchetypeArtifactId=starter", "-DarchetypeVersion=LATEST", "-DgroupId=com.flowlogix",
+                "-DartifactId=sample", "-Dversion=1.x-SNAPSHOT", "-DbaseType=payara", "-Dpackage=com.flowlogix.example",
+                "-DinteractiveMode=false" },
+                projectDirectory, System.out, System.err);
+    }
+}
