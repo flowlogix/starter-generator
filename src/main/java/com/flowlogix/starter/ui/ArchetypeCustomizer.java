4943cb7e32eb4a7d03e0ab052dd014f99a24394b
lprimak <lenny@flowlogix.com>
2024-10-07 04:43:20 -0500

    more bugs

--- src/main/java/com/flowlogix/starter/ui/ArchetypeCustomizer.java
+++ src/main/java/com/flowlogix/starter/ui/ArchetypeCustomizer.java
@@ -123,10 +123,7 @@ public class ArchetypeCustomizer implements Serializable {
                                 .replace("+", "%20")))
                 .collect(Collectors.joining(";"));
         var baseURL = Faces.getRequestBaseURL();
-        String proto = "X-Forwarded-Proto";
-        if (!Faces.isRequestSecure()
-                && ("https".equalsIgnoreCase(Faces.getRequestHeader(proto))
-                || "https".equalsIgnoreCase(Faces.getRequestHeader(proto.toLowerCase())))) {
+        if (Faces.isRequestSecure()) {
             var httpUrl = URI.create(baseURL).toURL();
             if (!httpUrl.getProtocol().endsWith("s")) {
                 baseURL = new URI(httpUrl.getProtocol() + "s", null, httpUrl.getHost(),
